36e0cce4c866458ddc9c825ba61aa7faeda75d55
justinbrown11 <96081581+justinbrown11@users.noreply.github.com>
2023-03-20 18:17:20 -0600

    quick change

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { IoIosBasketball } from 'react-icons/io';
 
 const Intro = () => {
 
@@ -6,6 +7,7 @@ const Intro = () => {
     <Link to="/home" style={{ textDecoration: 'none' }}>
         <div className="App">
             <header className="App-header">
+              <IoIosBasketball size={150}/>
                 <h1>Welcome to March Madness</h1>
                 <small style={{fontStyle: 'italic'}}>Click anywhere to continue</small>
             </header>
